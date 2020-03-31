47f23638f16a0ac25937436442ed36cf555aa186
Alex <alexx_88_88@mail.ru>
2020-03-31 21:36:41 +0300

    Object pool

---
+++ Assets/Sources/Shared/ObjectPool.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Craft_TZ.Shared
+{
+    public class ObjectPool<T>
+    {
+        private ConcurrentBag<T> pool;
+        private Func<T> objectGenerator;
+
+        public ObjectPool(Func<T> objectGenerator, int poolCapacity)
+        {
+            this.objectGenerator = objectGenerator;
+
+            pool = new ConcurrentBag<T>();
+
+            for (int i = 0; i < poolCapacity; i++)
+            {
+                pool.Add(objectGenerator());
+            }
+        }
+
+        public T GetObject()
+        {
+            T item;
+            if (pool.TryTake(out item))
+                return item;
+            return objectGenerator();
+        }
+
+        public void PutObject(T item)
+        {
+            pool.Add(item);
+        }
+    }
+}
