6146a7394508ff5e25d0273d1001e21834337c59
Alex <alexx_88_88@mail.ru>
2020-04-02 10:56:11 +0300

    CrystalPositionGenerators
    - 2 генератора позиций кристалов. используюя алгоритмы выборки координат из массива генерируют координаты, в которых должны быть сгенерированны кристалы;

---
+++ Assets/Sources/Model/CrystalPositionGenerators/ICrystalPositionGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Craft_TZ.Model.Crystal
+{
+    public interface ICrystalPositionGenerator
+    {
+        IReadOnlyCollection<Vector2> GenerateCrystalPositions(IEnumerable<Vector2> tileGenerationPositions);
+    }
+}
---
+++ Assets/Sources/Model/CrystalPositionGenerators/RandomCrystalPositionGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Craft_TZ.Shared.SelectionAlgorithms;
+using UnityEngine;
+
+namespace Craft_TZ.Model.Crystal
+{
+    public sealed class RandomCrystalPositionGenerator : ICrystalPositionGenerator
+    {
+        private RandomItemSelector selector;
+
+        public RandomCrystalPositionGenerator()
+        {
+            selector = new RandomItemSelector(5);
+        }
+
+        public IReadOnlyCollection<Vector2> GenerateCrystalPositions(IEnumerable<Vector2> tileGenerationPositions)
+        {
+            return selector.SelectItems(tileGenerationPositions).ToList().AsReadOnly();
+        }
+    }
+}
