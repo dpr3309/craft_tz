6c61fe1846ffee2b0eba67f1e9492e93ce757ef5
Alex <alexx_88_88@mail.ru>
2020-04-03 08:29:01 +0300

    simple player chip component

---
+++ Assets/Sources/View/PlayingFieldComponents/PlayerChip.cs
@@ -0,0 +1,18 @@
+﻿using Craft_TZ.Model.CoordinateModifiers;
+using UnityEngine;
+
+namespace Craft_TZ.View
+{
+    internal class PlayerChip : MonoBehaviour
+    {
+        [SerializeField]
+        private float speed;
+
+        private ICoordinateModifier coordinateModifier;
+
+        private void Update()
+        {
+            transform.position = coordinateModifier.TransformCoordinates(transform.position, speed);
+        }
+    }
+}
